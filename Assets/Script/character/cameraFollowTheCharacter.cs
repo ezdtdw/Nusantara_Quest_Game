3d88f1ad450992750dd50bb52ebbd3f6e489b634
FahranZandi <162786907+FahranZandi@users.noreply.github.com>
2025-03-11 21:49:49 +0800

    update1

--- Assets/Script/character/cameraFollowTheCharacter.cs
+++ Assets/Script/character/cameraFollowTheCharacter.cs
@@ -6,6 +6,9 @@ public class CameraFollow : MonoBehaviour {
     public Transform playerTransform;
     public Vector3 offset;
     public float smootSpeed = 0.125f;
+    
+
+    //test
 
     private void LateUpdate(){
         Vector3 desiredPosition = playerTransform.position + offset;
