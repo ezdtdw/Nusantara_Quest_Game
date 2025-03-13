2c2fe56af379d38177dc56b2317d8da7313b1354
FahranZandi <162786907+FahranZandi@users.noreply.github.com>
2025-03-13 12:20:56 +0800

    update8

--- Assets/Script/character/PlayerMovement.cs
+++ Assets/Script/character/PlayerMovement.cs
@@ -15,46 +15,79 @@ public class PlayerMovement : MonoBehaviour {
     public float jumpTime = 0.35f;
     public float jumpTimeCounter;
     private bool isJumping;
-
+    
+    // Animasi berjalan
+    public Sprite[] walkingSprites;
+    public Sprite[] idleSprites;
+    public float frameRate = 0.1f;
+    private int currentFrame;
+    private float frameTimer;
+    private int idleFrame;
+    private float idleTimer;
+    
+    // Public class untuk mengatur skala sprite
+    public Vector2 walkingSpriteScale = new Vector2(1, 1);
+    public Vector2 idleSpriteScale = new Vector2(1, 1);
 
     void Start() {
         playerRb.freezeRotation = true;
+        if (idleSprites.Length > 0) {
+            spriteRenderer.sprite = idleSprites[0]; // Set sprite awal ke idle
+        }
+        spriteRenderer.transform.localScale = idleSpriteScale; // Set skala awal
     }
 
-    void Update(){
+    void Update() {
         input = Input.GetAxisRaw("Horizontal");
-        if(input > 0){
+        if (input > 0) {
             spriteRenderer.flipX = false;
-        } else if(input < 0){
+        } else if (input < 0) {
             spriteRenderer.flipX = true;
         }
-
-        /*if (Input.GetKeyDown(KeyCode.Space)){
-            playerRb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-        }*/
-
+        
         isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckRadius, groundLayer);
 
-        if (isGrounded == true && Input.GetButtonDown("Jump")){
+        if (isGrounded == true && Input.GetButtonDown("Jump")) {
             isJumping = true;
             jumpTimeCounter = jumpTime;
             playerRb.linearVelocity = Vector2.up * jumpForce;
         }
-        if (Input.GetButton("Jump")  && isJumping == true){
-        
-            if(jumpTimeCounter > 0){
+        if (Input.GetButton("Jump") && isJumping == true) {
+            if (jumpTimeCounter > 0) {
                 playerRb.linearVelocity = Vector2.up * jumpForce;
                 jumpTimeCounter -= Time.deltaTime;
             } else {
                 isJumping = false;
             }
-        } 
-        if (Input.GetButtonUp("Jump")){
+        }
+        if (Input.GetButtonUp("Jump")) {
             isJumping = false;
         }
+
+        AnimateCharacter();
     }
-    void FixedUpdate()
-    {
+
+    void FixedUpdate() {
         playerRb.linearVelocity = new Vector2(input * speed, playerRb.linearVelocity.y);
+    }
+
+    void AnimateCharacter() {
+        if (input != 0) {
+            frameTimer += Time.deltaTime;
+            if (frameTimer >= frameRate) {
+                frameTimer = 0;
+                currentFrame = (currentFrame + 1) % walkingSprites.Length;
+                spriteRenderer.sprite = walkingSprites[currentFrame];
+                spriteRenderer.transform.localScale = walkingSpriteScale; // Set skala saat berjalan
+            }
+        } else {
+            idleTimer += Time.deltaTime;
+            if (idleTimer >= frameRate) {
+                idleTimer = 0;
+                idleFrame = (idleFrame + 1) % idleSprites.Length;
+                spriteRenderer.sprite = idleSprites[idleFrame]; // Animasi idle
+                spriteRenderer.transform.localScale = idleSpriteScale; // Set skala idle
+            }
+        }
     }
 }
